2be0b148e56af76cc6847c309ecba5498052b3a9
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-13 16:01:44 +0530

    added storage code

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "devrg" {
   name     = "Dev-RG"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
